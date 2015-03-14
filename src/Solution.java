cb48b7d5a24c821bec08d8441cc39e10620c93e3
Юрий <@Whizzpered.Dlink>
2015-03-14 22:36:17 +0400

    Merge origin/master
    Conflicts:
    	src/org/twisc/core/gameplay/Sprite.java

---
+++ src/Solution.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+public class Solution {
+    static Song[] songs ;
+    public static void main(String... args){
+        
+        Scanner in = new Scanner(System.in);
+        
+        int n = in.nextInt(),k = in.nextInt();
+        songs = new Song[n];
+        for(int i=0;i<n;i++){
+           songs[i].time = in.nextInt();
+           songs[i].timer= in.nextInt();
+           if(i<k)songs[i].endtime = songs[i].time +songs[i].timer;
+       else songs[i].endtime=min(i-k,i) + songs[i].timer;
+        }
+        
+        for(Song s: songs)System.out.println(s.endtime);
+        }
+    
+    static int min(int x, int y){
+        int min = songs[x].endtime;
+        for(int i=x;i<y;i++){
+            if(songs[i].endtime<min)min=songs[i].endtime;
+        }
+        return min;
+    }
+    
+    private class Song{
+        int timer, time, endtime;
+    }
+}
