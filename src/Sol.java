021e6c0d7c66b5123644504c104355244f64f689
Юрий <@Whizzpered.Dlink>
2015-03-14 19:25:26 +0400

    Merge origin/master
    Conflicts:
    	src/org/twisc/core/gameplay/Sprite.java

---
+++ src/Sol.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Юрий
+ */
+public class Sol {
+    public static void main(String... args){
+        Scanner in = new Scanner(System.in);
+        
+        int w = in.nextInt(), h = in.nextInt();
+        
+        
+        }
+        
+}
